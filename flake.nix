573288d9a2e9c20a6319846896929a0b147757a2
Emil Ivanichkov <emil.ivanichkov@gmail.com>
2024-08-11 13:45:27 +0300

    🔧 Init `nix` devshell

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+  };
+
+  outputs = inputs @ {
+    nixpkgs,
+    flake-parts,
+    ...
+  }:
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      systems = ["x86_64-linux"];
+      perSystem = {pkgs, ...}: {
+        devShells.default = with pkgs;
+          mkShell {
+            packages = [
+              jq
+            ];
+          };
+      };
+    };
+}
